1cee196adb9a555a3bbc6d20a777d4825aa190bb
NeeluAns1 <117790772+NeeluAns1@users.noreply.github.com>
2022-11-25 14:35:50 +0530

    Add files via upload

---
+++ projectrel.py
@@ -0,0 +1,42 @@
+import socket
+import subprocess
+import sys
+
+from datetime import datetime
+
+#Blank your screen
+
+subprocess.call('clear', shell=True)
+
+#Ask for input
+
+remoteServer = input("Enter a remote host to scan: ")
+remoteServerIP = socket.gethostbyname(remoteServer)
+
+#Print a nice banner with information on which host we are about to scan
+print(" " *60)
+print("Please wait, scanning remote host", remoteServerIP)
+print(" " *60)
+
+#Check the date and time the scan started
+t1 = datetime.now()
+
+#Using the range function to specify ports
+#Also we will do error handling
+
+try:
+    for port in range(1,5000):
+       sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+       result = sock.connect_ex((remoteServerIP, port))
+       if result ==0:
+           print("port {}:      open".format(port))
+           sock.close()
+except KeyboardInterrupt:
+    print("you pressed Ctrl+C")
+    sys.exit()
+except socket.gaierror:
+    print("Hostname could not be resovled. Exiting")
+    sys.exit()
+except socket.error:
+    print("Couldn't connect to server")
+    sys.exit()
